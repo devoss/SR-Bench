a683db1f01253206deef5df92bc9457df3cd3e8d
Chadwick DeVoss <netricate@gmail.com>
2012-01-16 14:20:28 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SrBench::Application.config.secret_token = '6177cdafdbc4fdf1be3394f8721678360f3ec1185b932bd298a14d1970880cc513404de4e8bd40cd842053a3c478dabf9324859da3cbc3456de9bd984f1ccb56'
